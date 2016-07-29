c0c8453b63a6e5b09d473fdc181e42aea4fe4434
Alex Defreitas <alexandreadef@gmail.com>
2016-07-29 17:16:29 -0400

    Added Ingredient Routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var cors = require('cors');
 
 
 
@@ -18,6 +19,95 @@ var ingredientSchema = {
 }
 
 var Ingredient = mongoose.model('Ingredient', ingredientSchema, 'ingredients');
+
+
+
+//---
+// Body parser
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+
+//---
+// Routes
+
+app.get('/', function (req, res) {
+  Ingredient.find(function (err, doc) {
+    res.send(doc);
+  })
+});
+
+app.get('/ingredients', function (req, res) {
+  console.log('hit /ingredients route');
+  Ingredient.find(function (err, doc) {
+    res.send(doc);
+  })
+});
+
+app.post('/addIngredient', function (req, res) {
+  console.log('hit /addIngredient route');
+  var newIngredient = new Ingredient({
+      name: req.body.name,
+      type: req.body.type,
+      cost: req.body.cost,
+      unit: req.body.unit,
+  });
+
+  newIngredient.save(function (err) {
+    if (err) {
+      console.log(err);
+      res.send(err);
+    } else {
+      console.log('added ingredient!');
+      Ingredient.find(function (err, doc) {
+        res.send(doc);
+      })
+    }
+  })
+});
+
+app.post ('/editIngredient', function (req, res) {
+  console.log('hit /editIngredient route');
+  Ingredient.update({
+    _id: req.body._id,
+    }, {
+      $set: {
+        name: req.body.name,
+        type: req.body.type,
+        cost: req.body.cost,
+        unit: req.body.unit,
+      }
+    },
+
+    function (err) {
+      if (err) {
+        console.log(err);
+        res.send(err);
+      } else {
+        Ingredient.find(function (err, doc) {
+          res.send(doc);
+        });
+      }
+    });
+});
+
+app.post ('/deleteIngredient', function (req, res) {
+  console.log('hit /deleteIngredient route');
+
+  Ingredient.remove({ _id: req.body._id }, function (err) {
+    if (err) {
+      console.log(err);
+      res.send(err);
+    }
+    else {
+      Ingredient.find(function (err, doc) {
+        res.send(doc);
+      });
+    }
+  });
+});
 
 
 
